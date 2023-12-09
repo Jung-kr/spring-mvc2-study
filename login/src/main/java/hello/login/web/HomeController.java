41fa9d301fd58e7da6568f6dc872e13bd2f1f63b
Jung-kr <jungwy98@gmail.com>
2023-12-09 20:24:35 +0900

    feat : 섹션 6. 로그인 처리1 - 쿠키, 세션 / HttpSession, @SessionAttribute, 타임아웃 설정

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -44,7 +45,7 @@ public class HomeController {
         return "loginHome";
     }
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) { //로그인 안한 사용자도 접속 가능해야 하기 때문에 required=false
 
         //세션 관리자에 저장된 회원 정보 조회
@@ -58,7 +59,8 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //서블릿이 제공하는 세션 HttpSession
+    //    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         //세션이 없으면 home
@@ -68,6 +70,20 @@ public class HomeController {
         }
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    //스프링이 제공하는 @SessionAttribute -> 이미 로그인된 사용자를 찾을 때 사용
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
         //세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
             return "home";
