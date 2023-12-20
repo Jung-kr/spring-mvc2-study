96e602ba4f8e5d97b5e89f2c907082a54c673a04
Jung-kr <jungwy98@gmail.com>
2023-12-20 12:06:15 +0900

    feat : 섹션 7. 로그인 처리2 - 필터, 인터셉터 / 스프링 인터셉터, ArgumentResolver

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,17 +1,48 @@
 package hello.login;
 
+import hello.login.web.Interceptor.LogInterceptor;
+import hello.login.web.Interceptor.LoginCheckInterceptor;
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    // LoginMemberArgumentResolver
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    // 스프링 로그 인터셉터
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error"
+                );
+    }
+
+    // 서블릿 로그 필터
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -20,7 +51,9 @@ public class WebConfig {
         return filterFilterRegistrationBean;
     }
 
-    @Bean
+
+    // 서블릿 로그인 필터
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
