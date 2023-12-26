737fdcdb200e2f6149ad9adeea3c34cb8f928c97
Jung-kr <jungwy98@gmail.com>
2023-12-26 18:01:22 +0900

    feat : 섹션 10. 스프링 타입 컨버터, 컨버전 서비스

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,36 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    //서블릿
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    //스프링 MVC가 제공하는 @RequestParam
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    //WebConfig에 등록한 StringToIpPortConverter 사용
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort  IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
+        return "ok";
+    }
+
+}
